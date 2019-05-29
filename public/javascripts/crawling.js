4fab0e0564e514ef25d902e478afd344030654a0
Hong seong hyun <shh6155@naver.com>
2019-05-30 00:02:49 +0900

    Mini_ranking5.30.1

---
+++ public/javascripts/crawling.js
@@ -0,0 +1,88 @@
+var request = require('request'),
+cheerio = require('cheerio');
+var url = "http://www.naver.com";
+var url2 = "https://www.daum.net/?t__nil_top=refresh";
+var fs = require('fs');
+var mysql = require('mysql');
+var ejs = require('ejs');
+/*
+    mysql> CREATE TABLE daum(
+    -> name char(40) not null,
+    -> num int PRIMARY KEY AUTO_INCREMENT not null);
+*/
+
+//mysql 정보
+var connection = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : '6155',
+    database : 'crawling',
+    
+  });
+
+//콘솔용 시간
+var date = new Date()
+date =`${date.getHours()}:${date.getMinutes()}`;
+
+//mysql 연결
+connection.connect();
+
+console.log('crawlinag start');
+
+//시간 간격으로 이벤트 실행
+var playloop= setInterval(function(){
+
+    //네이버 크롤링
+    connection.query('DELETE FROM ranking');
+    console.log('Naver테이블 초기화');
+
+    request(url, function (err, res, html) {
+        if (!err) {
+            var $ = cheerio.load(html);
+            var post =new Array();
+            for(var i =1; i <21; i++ ){
+                $(".ah_roll_area .ah_k").each(function(){
+                    var data = $(this);
+                    var data_txt =data.text();
+                    var data_split = data_txt.split(",");
+                    post[i]=([i]+"위 _    "+ data_split);
+                    i++;
+                });
+            };
+            for(var j = 1; j<21; j++){
+                connection.query('INSERT INTO ranking (name) values(?)',
+                    [post[j]])
+            };
+            console.log(post,`#####Naver 크롤링 저장 완료${[date]}######`);
+        };
+    });
+
+    //다음 크롤링
+    connection.query('DELETE FROM daum');
+    console.log('daum테이블 초기화');
+
+    request(url2, function (err, res, html) {
+        if (!err) {
+            var $ = cheerio.load(html);
+            var postdaum =new Array();
+            for(var i =1; i <11; i++ ){
+                $("span.txt_issue").each(function(){
+                    var data = $(this);
+                    var data_txt =data.text();
+                    var data_split = data_txt.split(",");
+                    postdaum[i]=([i]/2+"위 _    "+ data_split);
+                    i++
+                });
+            };
+            for(var j = 1; j<11; j++){
+                connection.query('INSERT INTO daum (name) values(?)',
+                    [postdaum[j*2]])
+            };
+            if(date=="4:33"){
+                clearInterval(playloop);
+            };
+            console.log(postdaum,`#####Daum크롤링 저장 완료${[date]}######`);
+        };    
+    });
+//1분 기준 재실행
+},60*1000);
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,62 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
 
+var connection = mysql.createConnection({
+  host : 'localhost',
+  user : 'root',
+  password : '6155',
+  database : 'crawling',
+  
+});
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-router.get('/naver', function(req, res, next) {
-  res.render('naver');
+  res.render('index', { title: 'Title' });
 });
 
+router.get('/melon', function(req, res, next) {
+  res.render('naver', { title: 'melon' });
+});
+router.get('/genie', function(req, res, next) {
+  res.render('naver', { title: 'genie' });
+});
+
+router.get('/naver', function(req, res, next) {
+  connection.query(`
+    SELECT  
+        *
+    FROM
+        ranking   
+    LIMIT 
+        0,20`,
+      function(error,results){
+        if(error){
+          console.log(`error : ${error.message}`);
+        }else{
+          res.render('ranking',{
+          rank : results,
+          title :'Naver Rankig'
+          });
+        };   
+      });
+});
+router.get('/daum', function(req, res, next) {
+  connection.query(`
+    SELECT  
+        *
+    FROM
+        daum   
+    LIMIT 
+        0,20`,
+      function(error,results){
+        if(error){
+          console.log(`error : ${error.message}`);
+        }else{
+          res.render('ranking',{
+          rank : results,
+          title :'Daum Rankig'
+          });
+        };   
+      });
+});
 module.exports = router;
