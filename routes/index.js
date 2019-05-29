6f26735bffc2835f80ee8bfb425e04593fa18641
Hong seong hyun <shh6155@naver.com>
2019-05-29 18:55:51 +0900

    mini_ranking 5.29.1

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/naver', function(req, res, next) {
+  res.render('naver');
+});
+
+module.exports = router;
